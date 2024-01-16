7de0299788eadc8d69a78329603d52b1f8acb732
Daniel Getz <dandgetz@gmail.com>
2024-01-15 21:38:14 -0600

    Added Batch File for Starting up Github, open AI, and the folders

---
+++ LLM_Startup_Environment.bat
@@ -0,0 +1,8 @@
+@echo off
+start explorer.exe "C:\Users\Dan\Documents\LLMDownloads\Complete"
+start explorer.exe "C:\Users\Dan\Documents\LLMDownloads\To Be Appended"
+
+
+start "" "C:\Users\Dan\AppData\Local\GitHubDesktop\GitHubDesktop.exe"
+
+start firefox.exe "https://chat.openai.com/"
