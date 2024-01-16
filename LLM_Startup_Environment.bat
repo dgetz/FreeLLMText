bf11758af3d4d4510065d650368689d71a4c45e8
Daniel Getz <dandgetz@gmail.com>
2024-01-15 21:54:05 -0600

    Changes to the Batch Script
    This specifically opens up the program in python for edits and changesd

--- LLM_Startup_Environment.bat
+++ LLM_Startup_Environment.bat
@@ -1,8 +1,13 @@
 @echo off
+:: This opens the two folder locations for moving files around
 start explorer.exe "C:\Users\Dan\Documents\LLMDownloads\Complete"
 start explorer.exe "C:\Users\Dan\Documents\LLMDownloads\To Be Appended"
 
-
+:: This opens GitHub desktop
 start "" "C:\Users\Dan\AppData\Local\GitHubDesktop\GitHubDesktop.exe"
 
+:: Open ChatGPT for programming questions
 start firefox.exe "https://chat.openai.com/"
+
+:: Open python
+python -m idlelib.idle "C:\Users\Dan\Documents\GitHub\FreeLLMText\Text_File_Appender.py"
